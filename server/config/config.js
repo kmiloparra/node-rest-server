05ceea21c5b516320b9366f514893d8b1eceabb9
cristian <cristian.parra@avaldigitallabs.com>
2019-09-22 11:12:39 -0500

    restsrever - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 // puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+    urlDB = 'mongodb+srv://kmilopa:nBn6JzL6IpGonRKc@cluster0-qctwv.mongodb.net/cafe';
+}
+
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,29 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const bodyParser = require('body-parser');
 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
-})
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
+
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-
-    res.json({ body })
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
-
 
 app.listen(process.env.PORT, () => {
-    console.log(`escuchando por el puerto 3000`);
+    console.log(`escuchando por el puerto ${ process.env.PORT }`);
 });
